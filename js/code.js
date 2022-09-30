1be04928ea9b5fffe0fb61c78d03a541b7e296c0
yogi-permana <47520535+yogi-permana@users.noreply.github.com>
2022-09-30 14:10:38 +0700

    add parallax

--- js/code.js
+++ js/code.js
@@ -6,7 +6,7 @@ $('.page-scroll').on('click', function(e){
 	
 	// tangkap elemen ybs
 	var elementujuan = $(tujuan);
-	var titikatas = elementujuan.offset().top - 80;
+	var titikatas = elementujuan.offset().top - 50;
 	
 	// pindahkan scroll
 	$('html').animate ({
@@ -14,4 +14,49 @@ $('.page-scroll').on('click', function(e){
 	}, 1500);
 	
 	e.preventDefault();
+});
+
+
+// parallax
+// about
+$(window).on('load', function(){
+	$('.pkiri').css({
+		'opacity': '1',
+		'transform': 'translate(0,0)'
+	});
+	$('.pkanan').css({
+		'opacity': '1',
+		'transform': 'translate(0,0)'
+	});
+});
+
+$(window).scroll(function() {
+	
+	var wScroll = $(this).scrollTop();
+	
+	// jumbotron
+	$('.jumbotron img').css({
+		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+	});
+	
+	$('.jumbotron h1').css({
+		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+	});
+	
+	$('.jumbotron p').css({
+		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+	
+	// portfolio
+	if ( wScroll > $('.portfolio').offset().top - 250) {
+		$('.portfolio .img-thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portfolio .img-thumbnail').eq(i).css({
+					'opacity' : '1',
+					'transform' : 'translate(0,0)'
+				});
+			}, 300 * (i+1));
+		});
+	}
+	
 });
