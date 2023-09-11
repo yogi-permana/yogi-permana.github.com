f14aec0fcddddbf12f6334c0cfc30fb6b6702773
yogi-permana <47520535+yogi-permana@users.noreply.github.com>
2023-09-11 10:09:13 +0700

    update portofolio dan penambahan fitur jam

--- js/script.js
+++ js/script.js
@@ -32,3 +32,21 @@ document.addEventListener(
   },
   false
 );
+
+// menampilkan jam
+function updateClock() {
+  const clock = document.getElementById("clock");
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+
+  const timeString = `${hours}:${minutes}`;
+
+  clock.textContent = timeString;
+}
+
+// Memperbarui jam setiap detik
+setInterval(updateClock, 1000);
+
+// Memanggil fungsi updateClock untuk pertama kali
+updateClock();
